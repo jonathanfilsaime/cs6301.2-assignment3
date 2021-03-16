0bdc3d504a8a1f3f02bfab1599c9fac6349e46a6
Jonathan <jonathanfilsaime@gmail.com>
2021-03-15 22:13:43 -0500

    algo is working

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,6 +11,8 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Main {
 
     public static Map<Integer, List<String>> knowledgeBase = new ConcurrentHashMap<Integer, List<String>>();
+    public static Map<Integer, List<String>> literalMap = new ConcurrentHashMap<Integer, List<String>>();
+    public static int keyIndex;
     public static int initialSize;
     public static boolean found = false;
 
@@ -20,7 +22,6 @@ public class Main {
         readFile("task4.in");
         initialSize = knowledgeBase.size();
         negateClauses();
-        knowledgeBase.forEach((c,p) -> System.out.println(c + "-" +p));
         resolution();
         System.out.println("------------------------------------------");
         System.out.println("------------------------------------------");
@@ -53,19 +54,21 @@ public class Main {
     }
 
     public static void negateClauses() {
+        keyIndex = knowledgeBase.size();
         knowledgeBase.forEach((in, cl) -> cl.forEach(lit -> negateLiteral(lit)));
-
+        knowledgeBase.putAll(literalMap);
     }
 
     public static void negateLiteral(String lit) {
-        int index = knowledgeBase.size() + 1;
+
         Character neg = '~';
         if (!lit.equalsIgnoreCase("{}")) {
             if (neg.compareTo(lit.charAt(0)) == 0 ) {
                 List<String> clause = new ArrayList<>();
                 clause.add(lit.substring(1));
                 clause.add("{}");
-                knowledgeBase.put(index, clause);
+                literalMap.put(keyIndex, clause);
+                keyIndex++;
             } else {
                 StringBuilder st = new StringBuilder();
                 st.append("~");
@@ -73,25 +76,21 @@ public class Main {
                 List<String> clause = new ArrayList<>();
                 clause.add(st.toString());
                 clause.add("{}");
-                knowledgeBase.put(index, clause);
+                literalMap.put(keyIndex, clause);
+                keyIndex++;
             }
         }
     }
 
     public static void resolution() {
         outerloop:
-        for (int inferredKey = initialSize + 1; inferredKey < knowledgeBase.size(); inferredKey++) {
+        for (int inferredKey = initialSize; inferredKey < knowledgeBase.size(); inferredKey++) {
             for (int key = 0; key < initialSize; key++){
                 for (int index = 0; index < knowledgeBase.get(key).size(); index++) {
-                    if (!knowledgeBase.get(key).get(index).equalsIgnoreCase("{}")
-                            && !knowledgeBase.get(key).get(index).equalsIgnoreCase("false")) {
-                        compare(key, inferredKey, index);
-                        if (found) {
-                            System.out.println("I WON BITCH");
-                            break outerloop;
-                        }
+                    if(found) {
+                        break outerloop;
                     } else {
-                        //do nothing
+                        compare(key, inferredKey, index);
                     }
                 }
             }
@@ -106,9 +105,7 @@ public class Main {
 
         if(match(key, inferredKey, index) && ((initialLitValue && !inferredLitValue) || (!initialLitValue && inferredLitValue))) {
             System.out.println("-----------------------");
-            System.out.println("WTF --->" + match(key,inferredKey, index));
             System.out.println("original :" + knowledgeBase.get(key).get(index) + " inferred :" + knowledgeBase.get(inferredKey).get(0));
-            System.out.println("-----------------------");
             resolve(key,inferredKey, index);
         }
     }
@@ -136,36 +133,33 @@ public class Main {
     public static void resolve(int key, int inferredKey, int index) {
         List<String> clause = new ArrayList<>();
         if (knowledgeBase.get(key).size() == 2) {
-            System.out.println("size is only 1");
+            System.out.println("clause is : " + knowledgeBase.get(key));
             clause.add("false");
             clause.add("{"+ key + ","+ inferredKey +"}");
-            System.out.println("clause is : " + clause);
-            knowledgeBase.put(key + 1, clause);
+            System.out.println("resolved clause is : " + clause);
+            int size = knowledgeBase.size();
+            knowledgeBase.put(size, clause);
             found = true;
         } else {
-            System.out.println("size is not 1");
+            System.out.println("clause is : " + knowledgeBase.get(key));
             clause = knowledgeBase.get(key);
-            clause.remove(index);
-
-            List<String> resolved = cleanUp(clause);
-            resolved.add("{"+ key + ","+ inferredKey +"}");
-            System.out.println("clause is : " + resolved);
-            knowledgeBase.put(key + 1, resolved);
+            List<String> resolved = createNewLiteral(clause, key, inferredKey, index);
+            System.out.println("resolved clause is : " + resolved);
+            int size = knowledgeBase.size();
+            knowledgeBase.put(size, resolved);
         }
-        System.out.println("BIG ---> " + knowledgeBase.get(key + 1));
     }
 
-    public static List<String> cleanUp(List<String> clause) {
-        List<String> newClause = new ArrayList<>(clause);
+    public static List<String> createNewLiteral(List<String> clause, int key, int inferredKey, int index) {
         List<String> indexToBeRemoved = new ArrayList<>();
-
-        for (int i = 0; i < newClause.size(); i++) {
-            if (newClause.get(i).substring(0,1).equalsIgnoreCase("{")) {
-                indexToBeRemoved.add(newClause.get(i));
+        indexToBeRemoved.add(clause.get(index));
+        for (int i = 0; i < clause.size(); i++) {
+            if (clause.get(i).substring(0,1).equalsIgnoreCase("{")) {
+                indexToBeRemoved.add(clause.get(i));
             }
         }
-        newClause.removeAll(indexToBeRemoved);
-        return newClause;
+        clause.removeAll(indexToBeRemoved);
+        clause.add("{"+ key + ","+ inferredKey +"}");
+        return clause;
     }
-
 }
